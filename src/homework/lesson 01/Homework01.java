afbd14d36c300fbb464b6cd6bdfc4cb5114784df
Alexander Malashkevich <mr.neelixs@gmail.com>
2021-02-02 23:01:09 +0300

    Homework lesson01

--- src/homework/lesson 01/Homework01.java
+++ src/homework/lesson 01/Homework01.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Homework01 {
+public class Homework01 {
+    public static void main(String[] args) {
+        System.out.println("                Рубаи");
+        System.out.println("Много лет размышлял я над жизнью земной");
+        System.out.println("Непонятного нет для меня под луной");
+        System.out.println("Мне известно что мне ничего не известно!");
+        System.out.println("Вот последняя правда открытая мной");
+        System.out.println("                            О.Хайам");
+    }
 }
